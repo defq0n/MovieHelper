2dd0f38fe9b35f5d0bc486c7c900a68db59f2733
Dustin <du.scholz@gmail.com>
2015-12-09 04:29:45 -0600

    sql files for add review and get movie

---
+++ src/java/com/moviehelper/beans/add-review.sql
@@ -0,0 +1,5 @@
+INSERT INTO "review"("title", "user", "text", "rating")
+    VALUES (SELECT "movie"."id" FROM "movie" WHERE "movie"."title" = ?), --title
+            SELECT "users"."id" FROM "users" WHERE "users"."username" = ?), --user
+            ?, --text
+            ?, --rating))
